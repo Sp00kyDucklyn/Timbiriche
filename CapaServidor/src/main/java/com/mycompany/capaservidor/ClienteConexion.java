3f0b025ea64a85109025fe4e91b5f97c17652fa7
Oscarpro11 <oscarito.tarazon@gmail.com>
2023-11-11 13:40:12 -0700

    Capa servidor

---
+++ CapaServidor/src/main/java/com/mycompany/capaservidor/ClienteConexion.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.capaservidor;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author oscar
+ */
+public class ClienteConexion extends Thread{
+    
+    private Server servidor;
+    private Socket socket;
+
+    public ClienteConexion(Server servidor, Socket socket) {
+        this.servidor = servidor;
+        this.socket = socket;
+    }
+    
+    
+    
+    
+    @Override
+    public void run() {
+        try {
+            ObjectInputStream obj= new ObjectInputStream(socket.getInputStream());
+            while(true){
+                Object object= obj.readObject();
+//                if(object instanceof movimientoDTO){
+//                    //Aqui se hace algo y luego se envia a todos
+//                    servidor.EnviarTodos(movimientoDTO);
+//                }
+                
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteConexion.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ClienteConexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    
+
+    
+
+    
+}
---
+++ CapaServidor/src/main/java/com/mycompany/capaservidor/Server.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.capaservidor;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author oscar
+ */
+public class Server {
+
+    List<Socket> listaS = new ArrayList<>();
+
+    public Server() throws IOException {
+        ServerSocket socketServidor = new ServerSocket(1542);
+
+        while (true) {
+            Socket conexionS = socketServidor.accept();
+            listaS.add(conexionS);
+            ClienteConexion clienteConexion = new ClienteConexion(this, conexionS);
+
+        }
+    }
+    public void EnviarTodos(Object object) throws IOException{
+        for (Socket socket : listaS) {
+            ObjectOutputStream objOS= new ObjectOutputStream(socket.getOutputStream());
+            objOS.writeObject(object);
+            objOS.flush();
+        }
+    }
+
+}
